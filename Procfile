10e4e3578e3877bd2d57a59d7b8ad2fe7e874741
aptrishu <code.rishus23@gmail.com>
2016-12-24 12:37:55 +0530

    Extras

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn webchat.wsgi --log-file -
--- settings.py
+++ settings.py
@@ -84,3 +84,17 @@ INSTALLED_APPS = (
     'django-jchat.documents',
     'django-jchat.jchat',
 )
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
